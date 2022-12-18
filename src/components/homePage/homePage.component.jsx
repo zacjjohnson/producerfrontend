b9ce18c0a682ca518da60c8c10722e9668f1100b
zacjjohnson <wecooked@gmail.com>
2022-12-17 21:53:29 -0500

    working on making mobile friendly

--- src/components/homePage/homePage.component.jsx
+++ src/components/homePage/homePage.component.jsx
@@ -7,38 +7,6 @@ import { AuthContext } from '../../context/auth.context';
 // import { useState } from 'react';
 
 
-
-
-const artistsWorkedWith = [
-    {
-        name: 'Hi-Fii',
-        id: 1
-    },
-    {
-        name: 'ZAYHHILL',
-        id: 2
-    },
-    {
-        name: 'Strick',
-        id: 3
-    },
-    {
-        name: 'KOZZA',
-        id: 4
-    },
-    {
-        name: 'PRIIMO',
-        id: 5
-    },
-    {
-        name: 'CANDYMAN D-MONEY',
-        id: 6
-    }
-]
-
-
-
-
 const HomePage = () => {
 
     const { isLoggedIn, user } = useContext(AuthContext);
